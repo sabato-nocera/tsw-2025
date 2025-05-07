3ecbb70d47e4c5e5fe60e9de824e472a3754054b
Sabato Nocera <52103278+sabato-nocera@users.noreply.github.com>
2025-05-07 13:28:34 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,4 +1,12 @@
-def _configure_database():
-    username = "admin"
-    password = "admin"
-    return db.connect(username, password)
+url = "http://example.com" # Sensitive
+url = "ftp://anonymous@example.com" # Sensitive
+url = "telnet://anonymous@example.com" # Sensitive
+
+import telnetlib
+cnx = telnetlib.Telnet("towel.blinkenlights.nl") # Sensitive
+
+import ftplib
+cnx = ftplib.FTP("ftp.example.com") # Sensitive
+
+import smtplib
+smtp = smtplib.SMTP("smtp.example.com", port=587) # Sensitive
