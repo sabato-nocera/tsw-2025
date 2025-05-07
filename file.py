4d81520a41c564f631537a83c474526df20805e0
Sabato Nocera <52103278+sabato-nocera@users.noreply.github.com>
2025-05-07 13:31:35 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,12 +1,6 @@
-url = "http://example.com" # Sensitive
-url = "ftp://anonymous@example.com" # Sensitive
-url = "telnet://anonymous@example.com" # Sensitive
+import requests
 
-import telnetlib
-cnx = telnetlib.Telnet("towel.blinkenlights.nl") # Sensitive
-
-import ftplib
-cnx = ftplib.FTP("ftp.example.com") # Sensitive
-
-import smtplib
-smtp = smtplib.SMTP("smtp.example.com", port=587) # Sensitive
+def fetch_data(endpoint):
+    url = "http://example.com/" + endpoint
+    response = requests.get(url)
+    return response.text
