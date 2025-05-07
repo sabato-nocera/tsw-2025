8a2b9f0aeb58982f5f5118bdcd87f175f24780c9
Sabato Nocera <52103278+sabato-nocera@users.noreply.github.com>
2025-05-07 13:45:25 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,8 +1,6 @@
 import sqlite3
 
-def get_user_by_name(name):
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    query = "SELECT * FROM users WHERE name = '" + name + "'"  # ❌ Unsafe concatenation
-    cursor.execute(query)
-    return cursor.fetchall()
+def get_user_by_name(value):
+    with connection.cursor() as cursor:
+        cursor.execute("{0}".format(value))
+        return cursor.fetchall()
